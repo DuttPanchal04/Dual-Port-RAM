b4e9aa007a2d1435b88bebf84ee577d2ca0a0dd6
Dutt Panchal <dattpanchal2904@gmail.com>
2025-06-27 14:03:11 +0530

    Update testbench.sv

--- 16x8_Dual_Port_RAM_With_Same_Clock/testbench.sv
+++ 16x8_Dual_Port_RAM_With_Same_Clock/testbench.sv
@@ -27,6 +27,9 @@ module tb;
 
   );
 
+  integer i;
+
+  // clock generation
   initial begin
 
     clk = 0;
@@ -34,6 +37,8 @@ module tb;
 
   end
 
+
+  // actual testcases
   initial begin
 
     $dumpfile("test_dpt.vcd");
@@ -53,14 +58,66 @@ module tb;
 
     re_a = 1'b0; re_b = 1'b0;
 
-    rst = 1'b0;
+    rst = 1'b1;
+
+
+    // check data in memory before any write. (Default value) or used this for loop block to check data after reset testcase also.
+    /*
+    for(i=0; i<16; i=i+1) begin
+      $display("data is %0b at %0b", dut.mem[i], i);
+    end
+    */
+
+
+    /*
+    // WWWWW-RRRRR (Write and Read by Port-A)
+    @(posedge clk);
+
+    we_a = 1; re_a = 0;
+
+    for (i = 0; i < 5; i = i + 1) begin
+      addr_a = i;
+      @(posedge clk);
+    end
+
+    we_a = 0; re_a = 1;
+
+    for (i = 0; i < 5; i = i + 1) begin
+      addr_a = i;
+      @(posedge clk);
+      //$display("Read A: addr = %0d, data = %0d", i, dout_a);
+    end
+
+    re_a = 0;
+	*/
+
+
+    /*
+    // WRWRWRWRWRWR... (through port-B)
+    for (i = 5; i < 10; i = i + 1) begin
+
+      addr_b = i;
+      we_b = 1; re_b = 0;
+      @(posedge clk);
+      we_b = 0; re_b = 1;
+      @(posedge clk);
+      //$display("Read B: addr = %0d, data = %0d", i, dout_b);
+
+    end
+
+    re_b = 0;
+
+*/
+
 
     // write data from both ports to different memory locations
 
 
+    /*    
     @(posedge clk);
     we_a = 1'b1;
     we_b = 1'b1; 
+	*/
 
 
     /*
@@ -100,18 +157,87 @@ module tb;
 
 
     // read data from memory locations
+    /*
     @(posedge clk);
     we_a = 1'b0;
     we_b = 1'b0;
     re_a = 1'b1;
     re_b = 1'b1;
+   */
+
+
+    // check data in memory after write.
+    /*for(i=0; i<16; i=i+1) begin
+      $display("data is %0b at %0b", dut.mem[i], i);
+    end
+   */
 
 
 
-    /* use this for check reset at any time ( clears all memory locations and both port's output pins)
+    // Read during write (different address)
+    /*
+    addr_a = 4'b0001; din_a = 8'hAA; din_b = 8'h0A; we_a = 1;
+    addr_b = 4'b0010; re_b = 1;
+
+    @(posedge clk);
+
+    we_a = 0; re_b = 0;
+*/
+
+
+    // Read during write (same address)
+    /*
+    addr_a = 4'd3; din_a = 8'hBB; we_a = 1;
+    addr_b = 4'd3; re_b = 1;
+    @(posedge clk);
+    we_a = 0; re_b = 0;
+*/
+
+
+    // Write to all 16 memory address locations using Port A
+  
+    for (i = 0; i < 16; i = i + 1) begin
+      @(posedge clk);
+      addr_a = i;
+      din_a  = i;
+      we_a   = 1;
+    end
+
+    @(posedge clk);
+    we_a = 0;
+
+    $display("Write completed!");
+
+    // Read from all 16 locations using Port B
+    
+    for (i = 0; i < 16; i = i + 1) begin
+      @(posedge clk);
+      addr_b = i;
+      re_b   = 1;
+    end
+
+    @(posedge clk);
+    re_b = 0;
+
+    $display("Read completed!");
+
+
+
+    /* use this for check reset at any time ( clears all memory locations and both port's output pins). First write something, then read it, then reset, and then read again.*/
+
+    /*
     #10;
-    rst = 1'b1;
+    rst = 1'b0;
     */
+
+
+    // check final memory data
+    #5;
+    for(i=0; i<16; i=i+1) begin
+      $display("data is %0b at %0b", dut.mem[i], i);
+    end
+
+
 
     #10;
 
